757a2d062c57d365ec5fd13b873a5a7b7bdc4840
Samson Adesanoye <adesanoye.samson@yahoo.com>
2022-09-05 12:38:29 +0100

    fix calculator bug

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -56,7 +56,9 @@ function result() {
 
    count.push(Number(currentNumber))
 
-   console.log(count);
+//    console.log(count);
+   document.getElementById("accumulator").innerHTML += `${document.getElementById("total").innerHTML} = `;
+   processResult();
 }
 
 function processAction(num1, num2, action) {
@@ -65,5 +67,53 @@ function processAction(num1, num2, action) {
         case "-" : return num1 - num2
         case "x" : return num1 * num2
         case "/" : return num1 / num2
+    }
+}
+
+function processResult() {
+    let action = null;
+    let current = null;
+
+    let total = 0;
+
+    if (isNaN(count[count.length - 1])) {
+        count.pop();
+    }
+
+    count.forEach(n => {
+        if(!isNaN(n)) {
+            if(current == null) {
+                current = n;
+            } else {
+                total += processAction(current, n, action)
+                current = null
+            }
+        } else {
+            action = n;
+            saveAction = n;
+        }
+    })
+
+    if(current != null) {
+        total = processAction(total, current, action)
+    }
+
+    document.getElementById("total").innerHTML = total.toString().substring(0, MAX_VISOR_CHAR)
+}
+
+function cleanCurrentEntry() {
+    document.getElementById("total").innerHTML = "";
+}
+
+function cleanAll() {
+    document.getElementById("total").innerHTML = "";
+    document.getElementById("accumulator").innerHTML = "";
+    count = [];
+}
+
+function percentage() {
+    let currentNumber = document.getElementById("total").innerHTML
+    if (currentNumber != "") {
+        document.getElementById("total").innerHTML = Number(currentNumber) / 100;
     }
 }
